1e4644714b6ce45a4f9507816e6cb5fad65fc69f
Yoshiteru Toyoda <6089more@gmail.com>
2021-07-18 13:02:21 +0000

    [Finish]課題3-bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,47 +3,50 @@ class BooksController < ApplicationController
   def index
     @book = Book.new#からのインスタンスをcreateに渡すためのもの
     @books = Book.all#すべてのブックスをデータから抜き取る
-    @book.user_id = current_user
-    @user = current_user
+    @book.user_id = current_user#ほんのユーザーはログインしている人のユーザーとなる
+    @user = current_user#今のユーザーはログインしているユーザー
   end
 
   def create
     @book = Book.new(book_params)#インデックスより送られてきsた空のインスタンスに変数を加える
-    @book.user_id = current_user.id
-    @user = current_user
-    if @book.save
-      redirect_to(book_path(@book.id))
+    @book.user_id = current_user.id#これにより、bookモデルとユーザーモデルをアソシエイトしてから投稿する
+    @user = current_user#ここでのユーザーとはログイン中のユーザー
+    if @book.save#本のデータ作成に成功したならば
+      redirect_to(book_path(@book.id))#本の詳細ページに移動します
       flash[:notice] = "Book was successfully created."#フラッシュ表示のためのコード
     else
-      @books = Book.all
-      render :index#レンダーにより直接インデックスアクションにアクセスする。そのままの状態で
+      @books = Book.all #render先に用意してないerror用のインスタンス変数を用意する必要がある。
+      render :index#レンダーにより直接インデックスアクションにアクセスする。そのままの状態でそのためrender先に足りないインスタンス変数はつけた咲いて書かないといけなくなる
     end
   end
 
   def show
-      @book_new = Book.new
+      @book_new = Book.new# 部分テンプレートのcreate機能を利用するため
       @book = Book.find(params[:id])#データベースよ取り込む
-      @user = @book.user
+      @user = @book.user#ここでのユーザーとは、本のタイトル感想を投稿したユーザということ
   end
-
   def edit
       @book = Book.find(params[:id])#データベースよりどの本なのかを特定する、インスタンス変数
+      if @book.user != current_user
+            redirect_to books_path
+      end
   end
+
 
   def update
       @book = Book.find(params[:id])
     if @book.update(book_params)#条件分岐により成功した場合と違う場合に条件を変える
        redirect_to(book_path(@book.id))#成功したら本の一覧ページに戻るはず
-       flash[:notice] = "Book was successfully updated."
+       flash[:notice] = "Book was successfully updated."#成功メッセージの際に表示
     else
-       render :edit
+       render :edit#アクションエディットにレンダーする
     end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to(books_path)
+    book = Book.find(params[:id])#消すための本をデータベースから照合する
+    book.destroy#本のデータ削除昨日
+    redirect_to(books_path)#消した後の移動先
     flash[:notice] = "Book was successfully destroyed."#フラッシュ表示のためのコード
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
- 
+
   devise_for :users  #一番上に置いておくのがよい、混ざると大変繰り返しバグの発生原因
   root :to => "homes#top"#ホーム画面
-  
-  get "/homes/about" => "homes#about"
-  
+
+  get "/home/about" => "homes#about"
+
   resources :books,only:[:new,:index,:edit,:show,:destroy,:update,:create]#ブックスに必要なルーティン
   resources :users,only:[:index,:show,:edit,:update]#新しくつくないといけないルーティン
 end
